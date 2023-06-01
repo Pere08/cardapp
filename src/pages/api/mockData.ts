c59a558de43c7ab9fb5a497139eb4e740e8bdf58
Pere Ripoll <pripoll91@gmail.com>
2023-06-01 20:14:39 +0200

    add mockData for cards

---
+++ src/pages/api/mockData.ts
@@ -0,0 +1,52 @@
+import { cardType } from "@/types/types"
+
+export const cards: cardType[] = [
+    {
+      title: "documento 1",
+      country: "spain",
+      version: "0.0.1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,  veniam, quis nost"
+    },
+    {
+      title: "documento 2",
+      country: "spain",
+      version: "0.0.2",
+      text: "Lorem ipsum dolor sit amet,"
+    },
+    {
+      title: "documento 3",
+      country: "japan",
+      version: "0.0.1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+    },
+    {
+      title: "documento 4",
+      country: "japan",
+      version: "0.0.2",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    },
+    {
+      title: "documento 5",
+      country: "germany",
+      version: "0.0.1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      title: "documento 6",
+      country: "germany",
+      version: "0.0.2",
+      text: "Lorem ipsum dolor sit amet."
+    },
+    {
+      title: "documento 7",
+      country: "canada",
+      version: "0.0.1",
+      text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+      title: "documento 8",
+      country: "canada",
+      version: "0.0.2",
+      text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+  ]
